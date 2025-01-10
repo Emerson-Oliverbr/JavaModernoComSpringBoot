773908f5227aa662644e37061efd6f083159d3c1
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-01-09 23:06:48 -0300

    Aprendendo os tipos primitivos

--- Seção-1-Fundamentos/2.Tipos Primitivos/Main.java
+++ Seção-1-Fundamentos/2.Tipos Primitivos/Main.java
@@ -1,5 +1,23 @@
 public class Main {
 	public static void main(String[] args) {
-		
+
+		//Aula sobre tipos primitivos
+		byte idade = 39;
+		short ano = 1985;
+		int populacaoCidade = 90000;
+		long populacaoMundial = 7800000000L;
+		float altura = 1.69f;
+		double salario = 2400.00;
+		boolean estudante = true;
+		char inicialNome = 'E';
+
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano: " + ano);
+		System.out.println("Populacao da Cidade: " + populacaoCidade);
+		System.out.println("Populacao do Mundial: " + populacaoMundial);
+		System.out.println("Altura: " + altura);
+		System.out.println("Salario: "+ salario);
+		System.out.println("Estudante? " + estudante);
+		System.out.println("Letra Inicial do Nome: " + inicialNome);
 	}
 }
