343d1cbc378259e7b10207d01574093a7c0d25fe
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-01-11 21:34:56 -0300

    Identificadors de variáveis

---
+++ Seção-1-Fundamentos/5.Identificadores/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+	public static void main(String[] args) {
+		/* 
+ 			Exemplos de identificadores Válidos
+ 				nome
+ 				idade
+ 				salarioMensal
+ 				_temp
+ 				$id
+		*/
+
+		/*
+			Exemplos de identificadores Inválidos
+				123abc "Começa com dígito"
+				a-b "Contém caractere não permitido"
+				class "É uma palavra reservada"
+		*/
+
+		var numero = 10.5D;
+		double numero2 = numero;
+
+		System.out.println("Numero 2 = " + numero2);
+	}
+}
