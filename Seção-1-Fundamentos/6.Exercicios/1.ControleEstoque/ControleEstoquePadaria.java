278affdf3a1c0c134381813afeca02d51bc83e44
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-01-11 22:24:04 -0300

    Add: Primeiros exercicios do curso

---
+++ Seção-1-Fundamentos/6.Exercicios/1.ControleEstoque/ControleEstoquePadaria.java
@@ -0,0 +1,12 @@
+public class ControleEstoquePadaria {
+	public static void main(String[] args) {
+
+		int quantidadeFarinha, quantidadeAcucar;
+
+		quantidadeFarinha = 10;
+		quantidadeAcucar = 5;
+
+		System.out.println("Quantidade de farinha = " + quantidadeFarinha);
+		System.out.println("Quantidade de Acucar = " + quantidadeAcucar);
+	}
+}
---
+++ Seção-1-Fundamentos/6.Exercicios/3.PromocaoAutomatica/PromocaoAutomatica.java
@@ -0,0 +1,18 @@
+public class PromocaoAutomatica {
+	public static void main(String[] args) {
+
+		byte idade = 39;
+		byte idade2 = 11;
+		short comprimentoCarro = 3 ;
+		char sexo = 'M';
+		int dataNascimento = 1985;
+		long popolacaoMundial = 8199386500L;
+		float notaPrimeiroSemestre = 9.8f;
+		double salarioMensal = 2400.0D;
+
+		int resultado = idade + idade2;
+
+		System.out.println("A soma das idades é:" + resultado);
+		System.out.println("Pessoa do sexo: " + sexo);
+	}
+}
