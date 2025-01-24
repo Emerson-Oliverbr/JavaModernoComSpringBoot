25693d09a5f6a4d4195590b8066d530607e4bec6
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-01-24 16:30:55 -0300

    Aprendendo sobre o modificador protected

---
+++ Seção-5-Principio-da-POO/9.Modificadores de Acesso Parte-2/FolhaDePagamento.java
@@ -0,0 +1,12 @@
+import code85.dados.Funcionario;
+import code85.dados.TestePackage;
+
+import java.util.Locale;
+
+public class FolhaDePagamento {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);		
+
+		TestePackage tp = new TestePackage ();
+	}
+}
---
+++ Seção-5-Principio-da-POO/9.Modificadores de Acesso Parte-2/code85/dados/Funcionario.java
@@ -0,0 +1,17 @@
+package code85.dados;
+
+public class Funcionario {
+	private String nome;
+	double salario;
+
+	private Funcionario(String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+		System.out.println("Nome: " + nome);
+		System.out.printf("Salario: R$ %.2f%n" , salario);
+	}
+
+	protected Funcionario() {
+		Funcionario func = new Funcionario("Laura", 5000.00);
+	}
+}
